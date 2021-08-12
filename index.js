d90fc92777bd7ee82def1953816ac10680aca290
CharlesTan44 <charles.tan0404@gmail.com>
2021-08-12 14:15:16 -0700

    testing Java script

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+// var, let, or const indicates a declaration
+// when you see '=' it indicates an assignment
+var firstName = "Charles";
+let lastName = "Tan";
+// You cannot reassign constant variables!
+const age = 21;
+// variable reassignments
+firstName = "Charles";
+lastName = "Tan";
+
+console.log('Hello ' + firstName + lastName + 'you are ' + age + 'years old');
